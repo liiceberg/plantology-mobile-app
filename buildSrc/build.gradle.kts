d0315488adbd071569c6c7d35a9620c2d36a3ffe
Aisylu Gimaletdinova <2aysylu2003@gmail.com>
2025-03-17 16:34:17 +0300

    up gradle version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.5.2")
+    implementation("com.android.tools.build:gradle:8.9.0")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     implementation("com.squareup:javapoet:1.13.0")
 }
